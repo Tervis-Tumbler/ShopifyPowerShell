d9507d1d03b8b20e317901d66f2133c55a89bff5
Adan Lozano <alozano@tervis.com>
2019-03-04 09:01:32 -0500

    adding product functions

--- ShopifyPowerShell.psm1
+++ ShopifyPowerShell.psm1
@@ -32,13 +32,13 @@ function Invoke-ShopifyAPIFunction{
     
     $Credential = Get-ShopifyCredential
 
-    $URI = "https://$($Credential.UserName):$($Credential.GetNetworkCredential().Password)@$ShopName.myshopify.com/admin/$Resource"
+    $URI = "https://$($Credential.UserName):$($Credential.GetNetworkCredential().Password)@$ShopName.myshopify.com/admin/$Resource.json"
 
-    $Response = if ($Body) {
-        Invoke-RestMethod -Method $HttpMethod -Credential $Credential -Uri $URI  -Body $Body
-    } else {
-        Invoke-RestMethod -Method $HttpMethod -Credential $Credential -Uri $URI
-    }
+ #   $Response = if ($Body) {
+ #       Invoke-RestMethod -Method $HttpMethod -Credential $Credential -Uri $URI  -Body $Body
+ #   } else {
+        Invoke-RestMethod -Credential $Credential -Uri $URI -Method $HttpMethod 
+#    }
     
     $Response
 }
@@ -46,9 +46,10 @@ function Invoke-ShopifyAPIFunction{
 function Get-ShopifyInventoryItems{
     [cmdletbinding()]
     param(
-    [Parameter(mandatory)]$ShopName,
-    [Parameter(mandatory)]$ItemIDsSeparatedByCommas
+        [Parameter(mandatory)]$ShopName,
+        [Parameter(mandatory)]$ItemIDsSeparatedByCommas
     )
+    #$ItemIDsSeparatedByCommas needs to be refactored.
 
     $HttpMethod = "Get"
     $Resource = "inventory_items.json?ids=$ItemIDsSeparatedByCommas"
@@ -59,12 +60,56 @@ function Get-ShopifyInventoryItems{
 function Get-ShopifyShop{
     [cmdletbinding()]
     param(
-    [Parameter(mandatory)]$ShopName
+        [Parameter(mandatory)]$ShopName
     )
 
     $HttpMethod = "Get"
-    $Resource = "shop.json"
+    $Resource = "shop"
 
     Invoke-ShopifyAPIFunction -HttpMethod $HttpMethod -Resource $Resource -ShopName $ShopName
+}
 
+function Get-ShopifyProducts{
+    [cmdletbinding()]
+    param(
+        [parameter(mandatory)]$ShopName
+    )
+
+    $HttpMethod = "Get"
+    $Resource = "products"
+
+    Invoke-ShopifyAPIFunction -HttpMethod $HttpMethod -Resource $Resource -ShopName $ShopName
+}
+
+function New-ShopifyProduct{
+    [cmdletbinding()]
+    param(
+        [parameter(mandatory)]$ShopName,
+        [parameter(mandatory)]$Title,
+        [parameter(mandatory)]$Description,
+        [parameter(mandatory)]$EBSItemNumber,
+        [parameter(mandatory)]$UPC,
+        [parameter(mandatory)]$Price,
+        $InventoryQuantity = 0
+    )
+
+    $HttpMethod = "Get"
+    $Resource = "products"
+
+    $Body = @"
+{
+    "product": {
+        "title": "$Title",
+        "body_html": "$Description",
+        "variants: [
+            {
+                "price":"$Price",
+                "sku":"$UPC",
+                "id":"$EBSItemNumber"
+                "inventory_quantity":$InventoryQuantity
+            }
+        ]
+    }
+}    
+"@
 }
